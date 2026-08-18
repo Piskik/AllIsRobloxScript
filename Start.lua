f9b2f1a3c14017d9e640c232d85b9b7e8661c8b9
Piskik <antonpioopeer@gmail.com>
2026-08-19 00:06:54 +0600

    Update Start.lua

--- Start.lua
+++ Start.lua
@@ -1,1 +1,14 @@
-Print("Hello,word")
+-- Этот код создаст синее уведомление в правом нижнем углу экрана игры Roblox
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "GitHub Успех!",
+    Text = "Ваш скрипт успешно загрузился и работает!",
+    Duration = 10,
+    Button1 = "Отлично"
+})
+
+-- Дополнительно сделаем персонажа супер-быстрым
+local p = game.Players.LocalPlayer
+if p and p.Character and p.Character:FindFirstChild("Humanoid") then
+    p.Character.Humanoid.WalkSpeed = 60
+end
+
